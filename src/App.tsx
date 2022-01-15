3bec13592085f189f72e2dee0e20e690ed9252fc
geofreybundala <bundalageofrey2@gmail.com>
2022-01-15 20:31:00 +0300

    📦 NEW: init project

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-10 min-h-screen flex items-center justify-center bg-cool-gray-700">
+      <h1 className="text-9xl font-black text-white text-center">
+        <span className="bg-gradient-to-r text-transparent bg-clip-text from-green-400 to-purple-500">
+          Hello there
+        </span>
+      </h1>
     </div>
   );
 }
