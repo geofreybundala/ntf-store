abe07010e4790222bcbb2d84573d6b4cdddbdef1
geofreybundala <bundalageofrey2@gmail.com>
2022-01-23 20:56:05 +0300

    👌 IMPROVE:  modfy product

---
+++ src/components/product/Show.tsx
@@ -0,0 +1,18 @@
+
+const Show = () => {
+return (
+    <div className="">
+        <div className="h-96">
+            <div 
+            className="bg-fixed basis-1/3 h-96" 
+            style={{
+                width: "100%",
+                height: "100%",
+                backgroundImage: `url("https://res.cloudinary.com/dojg0fuxn/image/upload/v1642316906/NTF-store/ashish-r-mishra-8Xhr_mrQozM-unsplash_eb2lgo.jpg")`}}>
+            </div>
+        </div>
+    </div>
+
+)
+}
+export default Show;
--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,9 +1,11 @@
 import Items from "../components/cards/Items";
 import Header from "../components/header/header";
+import Show from "../components/product/Show";
 const Product = () => {
    return (
     <div>
         <Header/>
+        <Show/>
         <Items title="Related Items"/> 
     </div>
    ) 
